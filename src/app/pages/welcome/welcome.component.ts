2f24291fba3d39ece4641ef8b9b80de4bf3f1032
Behnam <behnamborhani95@gmail.com>
2024-03-29 19:20:19 +0330

    train some basic concept of X6

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -24,6 +24,7 @@ export class WelcomeComponent implements AfterViewInit {
     nodes: [
       {
         id: 'node1', // String，可选，节点的唯一标识
+        shape: 'ellipse',
         x: 40, // Number，必选，节点位置的 x 值
         y: 40, // Number，必选，节点位置的 y 值
         width: 80, // Number，可选，节点大小的 width 值
@@ -32,6 +33,7 @@ export class WelcomeComponent implements AfterViewInit {
       },
       {
         id: 'node2', // String，节点的唯一标识
+        shape: 'rect',
         x: 160, // Number，必选，节点位置的 x 值
         y: 180, // Number，必选，节点位置的 y 值
         width: 80, // Number，可选，节点大小的 width 值
@@ -44,6 +46,11 @@ export class WelcomeComponent implements AfterViewInit {
       {
         source: 'node1', // String，必须，起始节点 id
         target: 'node2', // String，必须，目标节点 id
+        attrs: {
+          line: {
+            stroke: 'red',
+          },
+        },
       },
     ],
   };
@@ -59,7 +66,24 @@ export class WelcomeComponent implements AfterViewInit {
         color: '#F2F7FA',
       },
       grid: true,
+      panning: {
+        enabled: true,
+        // modifiers: 'shift',
+        eventTypes: ['leftMouseDown', 'rightMouseDown', 'mouseWheel'],
+      },
+      autoResize: true,
     });
+
+    // this.graph.isPannable();
+    this.graph.enablePanning();
+    // this.graph.disablePanning()
+    // this.graph.togglePanning();
+
+    // this.graph.zoom();
+    // this.graph.zoom(0.2);
+    // this.graph.zoom(-0.2);
+
+    this.graph.centerContent(); //The most common method is to align the center of the canvas content with the center of the viewport. Usage:
 
     this.graph.fromJSON(this.data);
 
